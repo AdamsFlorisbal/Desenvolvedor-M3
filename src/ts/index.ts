66d1781fe612e8229ad5c89f05f722c019c3b019
Adams Florisbal <adamsgawlinski@hotmail.com>
2022-12-02 23:16:51 -0300

    Captura filtros cor e tamanho

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,10 @@
 import { Product } from "./Product";
-//import fetchData from "./fetchData.js";
 const nameProduct = document.querySelector("#product");
 const colorProduct = document.querySelector(".filter-color");
 const sizeProduct = document.querySelector(".filter-size");
-const inputColor = document.querySelector(".filter-color");
-const btnSeeMore = document.querySelector("#btn-see-more");
 const listenerBtnMore = document.querySelector(".btn-more");
 var limitProduct = 9;
 var indexProduct = 0;
-
 
 async function fetchData<T>(url: string): Promise<T | null> {
   try {
@@ -24,15 +20,15 @@ async function fetchData<T>(url: string): Promise<T | null> {
 
 async function handleData() {
   const data = await fetchData<Product[]>("http://localhost:5000/products");
-  seeMore(data)
+  seeMore(data);
   renderProducts(data);
-  renderFilters(data);  
+  renderFilters(data);
 }
 
 //Renderiza produtos
 function renderProducts(data: Product[]) {
-  if (data) {    
-    for (indexProduct ; indexProduct < limitProduct; indexProduct++) {      
+  if (data) {
+    for (indexProduct; indexProduct < limitProduct; indexProduct++) {
       nameProduct.innerHTML += `
         <div class="product-list">
         <img src="${data[indexProduct].image}"/>
@@ -44,64 +40,80 @@ function renderProducts(data: Product[]) {
         <button class="btn-comprar">COMPRAR</button>
         </div>
         `;
-    }   
-  }  
- }
+    }
+  }
+}
 // Mostra mais produtos
- function seeMore(data: Product[]){
+function seeMore(data: Product[]) {
   listenerBtnMore.addEventListener("click", () => {
-    indexProduct = indexProduct;       
+    indexProduct = indexProduct;
     limitProduct += 3;
-    renderProducts(data);   
+    renderProducts(data);
   });
- }
+}
 // Renderiza todas os filtros possiveis
 function renderFilters(data: Product[]) {
-//pega todas as cores
+  //pega todas as cores
   var color: any[] | string = [];
   data.forEach((item) => {
     color.push(item.color);
   });
-//remove cores repetidas
+  //remove cores repetidas
   const arrayColor = color.filter(
     (item, index, arr) => arr.indexOf(item) == index
   );
-  
+
   //renderiza filtros por cor
-  arrayColor.forEach((color) => {
+
+  for (let i = 0; i < arrayColor.length; i++) {
+    console.log(arrayColor[i]);
     colorProduct.innerHTML += `
-  <div>
+    <div>  
     <input 
       type="radio"
       name="color"
-      value="${color}"
-      class="filter-color"
-      id="${color.toLowerCase()}" 
-      value="${color}" />
-    <label for="${color}">${color}</label>
-  </div>    `;
-  });
- 
+      onclick=""      
+      id="${arrayColor[i]}"      
+      value="${arrayColor[i].toLowerCase()}"    
+      class="input-filter-color"      
+       />
+       <h1 class="teste">${arrayColor[i]}</h1>
+       </div>          
+  `;
+  }
+  //captura os cliques de cor
+  var filterColor = document.querySelectorAll(".input-filter-color");
+  filterColor.forEach((input) =>
+    input.addEventListener("click", (event) => {
+      console.log(event.currentTarget.value);
+    })
+  );
 
   //renderiza filtros de tamanhos
-  var size : any[] | string = [];
+  var size: any[] | string = [];
   //Pega a matriz de tamanhos
   data.forEach((item) => {
-    size.push(item.size);    
+    size.push(item.size);
   });
   // Converte em um unico array
-  var arraySize = size.reduce((item, sub) => item.concat(sub), [])
+  var arraySize = size.reduce((item, sub) => item.concat(sub), []);
   // Remove tamanhos repetidos
-  const arrayColorFilter = arraySize.filter(
+  const arraySizeFilter = arraySize.filter(
     (item: String, index: Number, arr: any[]) => arr.indexOf(item) == index
-  ); 
+  );
   //renderiza tamanhos
-  arrayColorFilter.forEach((size: String) => {
-  sizeProduct.innerHTML += `
+  arraySizeFilter.forEach((size: String) => {
+    sizeProduct.innerHTML += `
     <button id="button-size" type="button">${size}</button>
-    `;} 
-)}
-
-
+    `;
+  });
+  //captura os cliques de tamanhos
+  const buttonSize = document.querySelectorAll("#button-size");
+  buttonSize.forEach((button) =>
+  button.addEventListener("click", (event) => {
+    console.log(event.currentTarget.innerText);
+  })
+  
+}
 
 document.addEventListener("DOMContentLoaded", handleData);
